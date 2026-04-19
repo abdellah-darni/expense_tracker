413258f1af74573591f4156b7e473f4b4ad941ad
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-04-19 15:05:21 +0100

    database helper

---
+++ lib/helper/database_helper.dart
@@ -0,0 +1,42 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('expenses.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB
+    );
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE expenses (
+        id TEXT PRIMARY KEY,
+        amount REAL,
+        description TEXT,
+        category TEXT,
+        date TEXT,
+        paymentMethod TEXT,
+        note TEXT,
+        isDeleted INTEGER DEFAULT 0
+      )
+    ''');
+  }
+
+}
